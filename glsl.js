06336c8f804fd1f111e3216220ee035d73df2400
Gaëtan Renaudeau <gre@zenexity.com>
2013-02-19 17:48:56 +0100

    use gl.UNPACK_FLIP_Y_WEBGL

--- glsl.js
+++ glsl.js
@@ -390,6 +390,7 @@ limitations under the License.
       var gl = this.gl;
       var texture = gl.createTexture();
       gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
